93c7736123c80a6b56efdbd6fc11a0338fde37b0
HeeL <parizhskiy@gmail.com>
2014-04-11 15:07:01 +0200

    new repo path

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -2,7 +2,7 @@
 set -e
 
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/var/www/puzzless/current
+APP_ROOT=/var/www/puzzless_api/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=heel
