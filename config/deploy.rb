f6eef41cd0a3b903ddfdfeb08a23d062282a42ee
HeeL <parizhskiy@gmail.com>
2014-04-12 18:28:37 +0200

    set deploy path

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:HeeL/puzzless_api.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/puzzless_api'
 
 # Default value for :scm is :git
 # set :scm, :git
